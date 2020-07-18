8b7af57999b159f7bb9bcf20c565630784e2b16b
HearyShen <jiayun.shen@foxmail.com>
2020-07-18 12:32:56 +0800

    minor updates

--- seu-login.sh
+++ seu-login.sh
@@ -10,4 +10,4 @@ echo "[Login] $username from $login_ip"
 login_url="https://w.seu.edu.cn:801/eportal/?c=Portal&a=login&callback=dr1003&login_method=1&user_account=%2C0%2C$username&user_password=$password&wlan_user_ip=$login_ip&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=jlh_me60&jsVersion=3.3.2&v=10107"
 
 response=$(curl -s $login_url)
-echo -e "[Response]: $response"
+echo -e "[Response] $response"
